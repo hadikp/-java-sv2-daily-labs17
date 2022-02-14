bebfe164a1c177eb6dd66db739d1dc2b9c729400
hadikp <hp20001025@gmail.com>
2022-02-14 21:19:57 +0100

    day01 jdbc

--- week17-lab-tasks/src/main/java/day01/ActorsRepository.java
+++ week17-lab-tasks/src/main/java/day01/ActorsRepository.java
@@ -1,4 +1,48 @@
 package day01;
 
+import org.mariadb.jdbc.internal.com.read.resultset.SelectResultSet;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ActorsRepository {
+
+    private DataSource dataSource;
+
+    public ActorsRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void saveActor(String name) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement stmt = connection.prepareStatement("Insert into actors(actor_name) values (?)")){
+            stmt.setString(1, name);
+            stmt.executeUpdate();
+
+        }
+        catch (SQLException sqe) {
+            throw new IllegalStateException("Can't connect database!", sqe);
+        }
+    }
+
+    public List<String> findActorsWithPrefix(String prefix) {
+        List<String> result = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection();
+            PreparedStatement stmt = connection.prepareStatement("Select actor_name from actors where actor_name like ?"))  {
+            stmt.setString(1, prefix + "%");
+
+            try(ResultSet rs = stmt.executeQuery()) {
+                while (rs.next()) {
+                    String actorName = rs.getString("actor_name");
+                    result.add(actorName);
+                }
+            }
+        }
+        catch (SQLException sqe) {
+            throw new IllegalStateException("Can't query!", sqe);
+        }
+        return result;
+    }
 }
---
+++ week17-lab-tasks/src/main/java/day01/Main.java
@@ -0,0 +1,28 @@
+package day01;
+
+import org.mariadb.jdbc.MariaDbDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MariaDbDataSource dataSource = new MariaDbDataSource();
+        try {
+            dataSource.setUrl("jdbc:mariadb://localhost:3306/movies-actors?useUnicode=true");
+            dataSource.setUserName("root");
+            dataSource.setPassword("ciprush11");
+        }
+        catch (SQLException sqe) {
+            throw new IllegalStateException("Can't reach database!", sqe);
+        }
+
+        ActorsRepository actorsRepository = new ActorsRepository(dataSource);
+        //actorsRepository.saveActor("Jack Doe");
+        System.out.println(actorsRepository.findActorsWithPrefix("Ja"));
+
+
+    }
+}
